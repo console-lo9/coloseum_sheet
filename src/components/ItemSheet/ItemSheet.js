d035868eade1e09b3fd5d4f0918fce32063b1fed
박중섭 <>
2022-02-18 01:55:08 +0900

    Add: 테이블에 MOCK_DATA 불러오기 완료

--- src/components/ItemSheet/ItemSheet.js
+++ src/components/ItemSheet/ItemSheet.js
@@ -3,24 +3,22 @@ import MOCK_DATA from "assets/MOCK_DATA.json";
 import styled from "styled-components";
 
 const ItemSheet = () => {
+  const keys = Object.keys(MOCK_DATA[0]);
+
   return (
     <div>
       <table>
         <thead>
-          <TableHead>데이터 출처</TableHead>
-          <TableHead>출고시 파일명</TableHead>
-          <TableHead>엑셀 행 순번</TableHead>
-          <TableHead>매핑 상태</TableHead>
-          <TableHead>주문서 양식</TableHead>
+          {keys.map((key, index) => (
+            <TableHead key={{ index }}>{key}</TableHead>
+          ))}
         </thead>
         <tbody>
           {MOCK_DATA.map((data, index) => (
             <tr key={index}>
-              <td>{data["데이터 출처"]}</td>
-              <td>{data["출고시 파일명"]}</td>
-              <td>{data["엑셀 행 순번"]}</td>
-              <td>{data["매핑 상태"]}</td>
-              <td>{data["주문서 양식"]}</td>
+              {Object.values(data).map((value) => (
+                <td>{value}</td>
+              ))}
             </tr>
           ))}
         </tbody>
