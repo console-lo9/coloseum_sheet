4864cb8081cab5bf0ff3473b8528ed9010e08cf8
Ubermensch0608 <melancholyee28@gmail.com>
2022-02-18 03:24:00 +0900

    Add:테이블 클릭시 해당 행과 열이 강조되는 기능 구현

--- src/components/ItemSheet/ItemSheet.js
+++ src/components/ItemSheet/ItemSheet.js
@@ -1,44 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
 import MOCK_DATA from "assets/MOCK_DATA.json";
 import styled from "styled-components";
+import { useSelector } from "react-redux";
 
 const ItemSheet = () => {
+  const [clickedId, setClickedId] = useState();
+  const [clickedRowId, setClickedRowId] = useState();
+  const tableData = useSelector((state) => state.data.tableData);
+
   const keys = Object.keys(MOCK_DATA[0]);
 
+  const isCheckedHandler = (event) => {
+    const isChecked = event.target.checked;
+    console.log(isChecked);
+  };
+
+  const setHighLightHandler = (event) => {
+    const clickedId = Number(event.target.id);
+    setClickedId(clickedId);
+  };
+
+  const setRowHighLightHandler = (event) => {
+    const clickedTrId = Number(event.target.parentElement.id);
+    setClickedRowId(clickedTrId);
+  };
+
   return (
-    <div>
-      <table>
-        <thead>
-          {keys.map((key, index) => (
-            <TableHead key={{ index }}>{key}</TableHead>
-          ))}
-        </thead>
+    <TableWrapper>
+      <Table>
+        <Thead>
+          <tr>
+            {keys.map((key, index) => (
+              <Th key={index}>
+                <div>
+                  <span>{key}</span>
+                  <span>
+                    <input type="checkbox" onChange={isCheckedHandler} />
+                  </span>
+                </div>
+              </Th>
+            ))}
+          </tr>
+        </Thead>
         <tbody>
-          {MOCK_DATA.map((data, index) => (
-            <Tr key={index}>
-              {Object.values(data).map((value) => (
-                <Td>{value}</Td>
+          {tableData.map((data, index) => (
+            <Tr
+              key={index}
+              id={index}
+              onClick={setRowHighLightHandler}
+              clickedId={clickedRowId}
+            >
+              {Object.values(data).map((value, index) => (
+                <Td
+                  key={index}
+                  id={index}
+                  onClick={setHighLightHandler}
+                  clickedId={clickedId}
+                >
+                  {value}
+                </Td>
               ))}
             </Tr>
           ))}
         </tbody>
-      </table>
-    </div>
+      </Table>
+    </TableWrapper>
   );
 };
 
-const TableHead = styled.th`
-  padding-right: 10px;
+const TableWrapper = styled.div`
+  overflow: auto;
+`;
+
+const Table = styled.table`
+  overflow: auto;
+`;
+
+const Thead = styled.thead``;
+
+const Th = styled.th`
+  padding: 10px;
+  outline: 1px solid black;
+  text-align: center;
+  white-space: nowrap;
+
+  div {
+    display: flex;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+    margin-right: 5px;
+  }
 `;
 
 const Tr = styled.tr`
   padding: 100px;
+  background-color: ${(props) => props.clickedId === props.id && "#D3DEDC"};
 `;
 
 const Td = styled.td`
-  padding-right: 20px;
+  padding: 15px;
   text-align: center;
-  outline: solid black;
+  outline: 1px solid black;
+  white-space: nowrap;
+
+  background-color: ${(props) => props.clickedId === props.id && "#D3DEDC"};
 `;
 
 export default ItemSheet;
