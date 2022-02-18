e6f40e9c2c596e32ccb7889482146a7bbe7eb4fe
ttaerrim <ltr0121@naver.com>
2022-02-19 03:59:22 +0900

    Modify: Color 상수 관리

---
+++ src/constants/color.js
@@ -0,0 +1,12 @@
+export const RED = "#ca0f0f";
+export const YELLOW = "#ca9d07";
+export const BLUE = "#0f58ca";
+export const LIGHTBLUE = "#e7f1ff";
+
+export const BLACK = "#000000";
+export const GRAY1 = "#3b3b3b";
+export const GRAY2 = "#707070";
+export const GRAY3 = "#c4c4c4";
+export const GRAY4 = "#e0e0e0";
+export const GRAY5 = "#f6f6f6";
+export const WHITE = "#fff";
